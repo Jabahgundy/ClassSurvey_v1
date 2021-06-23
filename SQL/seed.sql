0a9cd4ecaeb354a190bfbf0fc2ad530e362b8a66
Jabah Gundy <gundy157@outlook.com>
2021-06-23 14:44:33 -0400

    FirstCommit

---
+++ SQL/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO rankings
+    (ranking_title)
+VALUES
+    ('Poor'),
+    ('Okay'),
+    ('Good'),
+    ('Great'),
+    ('Awesome'),
+    ('Unranked');
+
+INSERT INTO topics
+    (topic_name, topic_score)
+VALUES
+    ('HTML', 5),
+    ('CSS', 4),
+    ('Javascript', 2),
+    ('PostgreSQL', 3),
+    ('Nodejs', 1),
+    ('Express', 6);
---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const http = require('http')
+const hostname = 'localhost';
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+const es6Renderer = require('express-es6-template-engine');
+
+app.engine('html', es6Renderer);
+app.set('views', './views');
+app.set('view engine', 'html');
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Listening at http://${hostname}:${port}`);
+});
+
+const rootController = require('./routes/index');
+
+app.use('/', rootController);
